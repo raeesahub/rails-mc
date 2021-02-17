12021bf2ae81176d8febb88db6ea021a720db9be
Raeesa Qureshi <qureshi.raeesa@gmail.com>
2021-02-17 19:36:59 +0000

    dose model with validations

--- app/models/dose.rb
+++ app/models/dose.rb
@@ -1,4 +1,8 @@
 class Dose < ApplicationRecord
+  validates :description, presence: true
   belongs_to :cocktail
   belongs_to :ingredient
+
+  validates :cocktail, uniqueness: { scope: :ingredient }
+  # validates :ingredient, uniqueness: { scope: :cocktail }
 end
