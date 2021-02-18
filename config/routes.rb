825158381516ce8e029cbf2ee3ed30f9b619a4a1
Raeesa Qureshi <qureshi.raeesa@gmail.com>
2021-02-18 19:50:14 +0000

    tidied up routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :cocktails do
-    resources :reviews
-    resources :doses
+  resources :cocktails, only: [:index, :show, :new, :create] do
+    resources :doses, only: [:create]
+    resources :reviews, only: [:create]
   end
-
-  resources :doses, only: :destroy
+  resources :doses, only: [:destroy]
 end
